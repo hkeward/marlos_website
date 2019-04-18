413a14daad08e0aa363d95309cadc8cd6a7ad7a1
Heather Ward <heather.ward13@gmail.com>
2019-04-17 23:29:30 -0400

    Add initial styles

--- marlos/src/main/webapp/get_rooms.js
+++ marlos/src/main/webapp/get_rooms.js
@@ -1,5 +1,9 @@
+function toggle() {
+    var room_description = document.getElementById('description');
+    room_description.style.display = room_description.style.display === 'none' ? '' : 'none';
+}
+
 var request = new XMLHttpRequest();
-console.log('test');
 
 
 request.open('GET', 'rest/rooms/first_room', true);
@@ -10,16 +14,29 @@ request.onload = function() {
     console.log(data);
 
     if (request.status >= 200 && request.status < 400) {
-        const card = document.createElement('div');
-        card.setAttribute('class', 'card');
+        const room_name = document.createElement('div');
+        room_name.setAttribute('class', 'room_name');
+        room_name.setAttribute('onclick', 'toggle();');
 
         const h1 = document.createElement('h1');
         h1.textContent = data.roomName;
 
-        container.appendChild(card);
-        card.appendChild(h1)
+
+        container.appendChild(room_name);
+        room_name.appendChild(h1);
+
+
+        const description = document.createElement('div');
+        description.setAttribute('id', 'description');
+        description.setAttribute('style', 'display: none');
+
+        const p = document.createElement('p');
+        p.textContent = data.description;
+
+        container.appendChild(description);
+        description.appendChild(p);
     } else {
-        console.log('error')
+        console.log('error');
     }
 };
 
