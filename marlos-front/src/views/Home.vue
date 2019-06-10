377c795107e37581042cf070941d6ce6cee260c6
Heather Ward <heather.ward13@gmail.com>
2019-06-10 19:23:25 -0400

    Add basic how-to information

--- marlos-front/src/views/Home.vue
+++ marlos-front/src/views/Home.vue
@@ -1,7 +1,58 @@
 <template>
 	<div class="home">
-		<h1>Hello</h1>
-		<p>This is the landing page! Welcome.</p>
+		<h1>
+			Hello
+		</h1>
+		
+		<p>
+			This is the landing page! Welcome.
+		</p>
+
+		<br />
+
+		<h2>
+			Using the website	
+		</h2>
+
+		<h3>
+			Rooms
+		</h3>
+
+		<p>
+			This is the page where things happen!
+			<br />
+			You can try the following:
+			<ul>
+				<li>
+					Rooms
+					<br />
+					Click on one of the tags in Rooms to filter by that tag
+				</li>
+				<li>
+					Rooms >> {click any room}
+					<br />
+					Click the 'More info' button when viewing a room to see things like its rating, environment, and whether or not darkvision or a grid are required. Open this menu while editing to change these values!
+				</li>
+				<li>
+					Rooms >> {Create new room button}
+					<br />
+					Create a new room using the currently super crappy new room form! Then, add the information that I haven't included in the form by navigating to the new room and clicking 'Edit'. Or, delete it and see it disappear!
+				</li>
+			</ul>
+		</p>
+
+		<br />
+
+		<h3>
+			Achtung
+		</h3>
+
+		<p>
+			Login is not currently functional! This means that literally any person on the internet can come in and delete the entire database. It's pretty unlikely that anyone will randomly find this page, but still -- until login becomes functional, **DO NOT PUT ANYTHING IN HERE THAT YOU DO NOT HAVE BACKUPS OF ELSEWHERE!!**
+			<br />
+			<br />
+			I am currently making a backup of the rooms database every hour, so if something is lost it's possible I still have it; let me know ASAP and I will try to retrieve it. Basically don't use this website 'for real' though until I have implemented login.
+		</p>
 	</div>
 </template>
 
