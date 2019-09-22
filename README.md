fc1dcc751b35374092f6ce1ab99117a3dafbfce3
Heather Ward <heather.ward13@gmail.com>
2019-09-22 13:04:12 -0400

    Add basic authentication with Keycloak
    Currently, the entire app is protected (visiting any page will redirect
    to kc login before the vue app is loaded). The idea is to have some
    unsecured pages that can be loaded (Home, About), then have only pages
    like rooms etc. protected by kc.

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 A website and tools used to organize D&D encounters.
 
+
+## Current setup
+
+Keycloak 6.0.1
+Wildfly 10.0.0 with Keycloak adapter
+
+nginx
--- marlos-front/src/main.js
+++ marlos-front/src/main.js
@@ -1,10 +1,35 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import * as Keycloak from 'keycloak-js'
+//
+// let keycloakScript = document.createElement('script');
+// keycloakScript.setAttribute('src', 'https://keycloak.heatherward.dev/auth/js/keycloak.js');
+// document.head.appendChild(keycloakScript);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+let keycloakConfig = {
+    "realm": "marlos",
+    "url": "https://keycloak.heatherward.dev/auth",
+    "clientId": "marlos-front"
+};
+
+let keycloak = new Keycloak(keycloakConfig);
+
+keycloak.init({ onLoad: 'login-required' }).success((auth) => {
+  if(!auth) {
+      console.log("Auth doesn't exist");
+    // window.location.reload();
+  } else {
+    console.log("Authenticated");
+  }
+
+  new Vue({
+    router,
+    render: h => h(App)
+  }).$mount('#app')
+}).error(() => {
+  console.log("Authentication failed");
+});
+
