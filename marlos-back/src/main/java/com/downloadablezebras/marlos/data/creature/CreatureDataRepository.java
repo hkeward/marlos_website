76112664c9dce8c56de6478bc99843c97851ff6d
Heather Ward <heather.ward13@gmail.com>
2020-09-05 11:52:45 -0400

    Allow creature that is associated with a room to be deleted
    The creature is first removed from all associated rooms, then it is
    deleted.
    I should probably emit a warning when this happens (e.g. when the user
    clicks delete, first query to find a list of associated rooms and say
    HEY ARE YOU SURE you will be deleting the creature from the following
    rooms etc. Same deal for edit perhaps..?

--- marlos-back/src/main/java/com/downloadablezebras/marlos/data/creature/CreatureDataRepository.java
+++ marlos-back/src/main/java/com/downloadablezebras/marlos/data/creature/CreatureDataRepository.java
@@ -3,5 +3,4 @@ package com.downloadablezebras.marlos.data.creature;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CreatureDataRepository extends JpaRepository<Creature, Long> {
-
 }
